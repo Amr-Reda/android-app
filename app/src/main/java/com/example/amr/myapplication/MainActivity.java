2c4aabe2b19250ba4de4ce1a2cdb7aa1223b3d9d
Amr_Reda <amrreda547@gmail.com>
2018-03-12 20:41:56 +0200

    update1

--- app/src/main/java/com/example/amr/myapplication/MainActivity.java
+++ app/src/main/java/com/example/amr/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.amr.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+       // TextView name = (TextView)findViewById(R.id.name);
+
+        TextView na = findViewById(R.id.name);
+        na.setText("sdbds");
+        CharSequence c =  na.getText();
+
+        ImageView img = findViewById(R.id.img);
+        img.setImageResource(R.drawable.img1);
+
+        EditText editText =findViewById(R.id.enter_name);
+        String var = editText.getText().toString();
+
     }
 }
