5cda27e66229baa431ebaf432bc115cd1a608d4d
Amr_Reda <amrreda547@gmail.com>
2018-03-13 18:41:13 +0200

    13/3

--- app/src/main/java/com/example/amr/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/amr/myapplication/Main2Activity.java
@@ -2,6 +2,7 @@ package com.example.amr.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second);
+
+        String retrieveData = getIntent().getStringExtra("var");
+        Toast.makeText(Main2Activity.this , retrieveData+" Hello " , Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/amr/myapplication/MainActivity.java
+++ app/src/main/java/com/example/amr/myapplication/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.amr.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,14 +23,29 @@ public class MainActivity extends AppCompatActivity {
        // TextView name = (TextView)findViewById(R.id.name);
 
         TextView na = findViewById(R.id.name);
-        na.setText("sdbds");
+//        na.setText("sdbds");
         CharSequence c =  na.getText();
 
-        ImageView img = findViewById(R.id.img);
-        img.setImageResource(R.drawable.img1);
+//        ImageView img = findViewById(R.id.img);
+//        img.setImageResource(R.drawable.img1);
 
-        EditText editText =findViewById(R.id.enter_name);
-        String var = editText.getText().toString();
+          editText =findViewById(R.id.enter_name);
+
+        Button click = findViewById(R.id.btn);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String var = editText.getText().toString();
+
+//                Toast.makeText(MainActivity.this, var , Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this ,Main2Activity.class);
+                intent.putExtra("var" , var);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
 
     }
 }
